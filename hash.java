a0ffddddb2c91e80975a5baba2f6420e9e56b530
nickroh <roh517@naver.com>
2022-02-10 20:59:49 -0600

    up

--- hash.java
+++ hash.java
@@ -1,88 +1,88 @@
 import java.util.*;
 
-interface hash<K, V>{
+interface hash<K, V> {
 
 }
 
-class hashmap<K,V> implements hash<K,V>{
-    
-    class Node<K, V>{
+class hashmap<K, V> implements hash<K, V> {
+
+    class Node<K, V> {
         K key;
         V value;
 
-        Node(K get_key, V get_value){
+        Node(K get_key, V get_value) {
             key = get_key;
             value = get_value;
         }
 
-        K get_key(){
+        K get_key() {
             return key;
         }
 
-        V get_value(){
+        V get_value() {
             return value;
         }
-        
+
     }
+
     class Link<K, V> {
-        Link(K key, V value){
+        Link(K key, V value) {
             list.add(new Node(key, value));
         }
 
-        void add(K key, V value){
+        void add(K key, V value) {
             list.add(new Node(key, value));
         }
 
-        void remove(int index){
+        void remove(int index) {
             list.remove(index);
         }
 
-        int size(){
+        int size() {
             return list.size();
         }
 
-        V get_value(int index){
+        V get_value(int index) {
             return (V) list.get(index).get_value();
         }
 
-        K get_key(int index){
+        K get_key(int index) {
             return (K) list.get(index).get_key();
         }
 
-        LinkedList<Node> list= new LinkedList<>();
+        LinkedList<Node> list = new LinkedList<>();
     }
 
-    
     private Link[] array;
 
-    hashmap(){
+    hashmap() {
         array = new Link[20];
     }
 
-    private int get_hash(K key){
+    private int get_hash(K key) {
         int index = key.hashCode() % 7;
 
         return index;
     }
 
-    public void add(K key, V value){
+    public void add(K key, V value) {
         int index = get_hash(key);
-        if(array[index]==null){
-            array[index]=new Link(key ,value);
-        }else{
-            array[index].add(key,value);
+        if (array[index] == null) {
+            array[index] = new Link(key, value);
+        } else {
+            array[index].add(key, value);
         }
     }
 
-    public void remove(K key){
+    public void remove(K key) {
         int index = get_hash(key);
         int size = array[index].size();
 
-        if(size == 1){
+        if (size == 1) {
             array[index] = null;
-        }else{
-            for(int i=0;i<array[index].size();i++){
-                if(((hashmap.Node) array[index].list.get(i)).get_key() == key ){
+        } else {
+            for (int i = 0; i < array[index].size(); i++) {
+                if (((hashmap.Node) array[index].list.get(i)).get_key() == key) {
                     array[index].remove(i);
                 }
             }
@@ -94,17 +94,30 @@ class hashmap<K,V> implements hash<K,V>{
 
         int index = get_hash(key);
         int size = array[index].size();
+
         
-        if(size == 1){
-            return (V) array[index].get_value(0);
-        }else{
-            for(int i=0;i<array[index].size();i++){
-                if(((hashmap.Node) array[index].list.get(i)).get_key() == key ){
+        try {
+            for (int i = 0; i < array[index].size(); i++) {
+                if (((hashmap.Node) array[index].list.get(i)).get_key() == key) {
                     return (V) array[index].get_value(i);
                 }
             }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+
         return null;
+
     }
 
+}
+
+class elementException extends Exception {
+    public elementException(String a) {
+        super(a);
+    }
+
+    public elementException() {
+    }
 }
--- main.java
+++ main.java
@@ -1,8 +1,30 @@
 public class main {
     public static void main(String[] args){
         hashmap h = new hashmap();
-        h.add("first", 2);
+        h.add("1", 2);
+        h.add("2", 3);
+        h.add("3", 4);
+        h.add("4", 5);
+        h.add("5", 6);
+        h.add("6", 7);
+        h.add("7", 8);
+        h.add("11", 9);
+        h.add("22", 10);
+        h.add("33", 11);
+    
 
-        System.out.println(h.get("first"));
+        System.out.println(h.get("1"));
+        System.out.println(h.get("3"));
+        System.out.println(h.get("4"));
+        System.out.println(h.get("5"));
+        System.out.println(h.get("6"));
+        System.out.println(h.get("7"));
+        System.out.println(h.get("2"));
+        System.out.println(h.get("11"));
+        
+        System.out.println(h.get("22"));
+   
+        System.out.println(h.get("33"));
+        System.out.println(h.get("44"));
     }
 }
